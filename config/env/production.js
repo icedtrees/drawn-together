441f37c32c451e2ff8dfae831c2197a4c95d6fcd
Leo Huang <crazysheep2302@gmail.com>
2015-09-16 11:20:56 +1000

    Change production port to 80

--- config/env/production.js
+++ config/env/production.js
@@ -6,7 +6,7 @@ module.exports = {
     privateKey: './config/sslcerts/key.pem',
     certificate: './config/sslcerts/cert.pem'
   },
-  port: process.env.PORT || 8443,
+  port: process.env.PORT || 80,
   db: {
     uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
