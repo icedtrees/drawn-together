ba8abd912de43e719f79215b1f9305d079985c87
cloudier <cldtu@yahoo.com.au>
2015-10-08 05:44:56 +1100

    Fixed changes in light of discussion on UI.

--- modules/users/client/config/users.client.routes.js
+++ modules/users/client/config/users.client.routes.js
@@ -34,6 +34,10 @@ angular.module('users').config(['$stateProvider',
         url: '/authentication',
         templateUrl: 'modules/users/client/views/authentication/authentication.client.view.html'
       })
+      .state('authentication.signup', {
+        url: '/signup',
+        templateUrl: 'modules/users/client/views/authentication/signup.client.view.html'
+      })
       .state('authentication.signin', {
         url: '/signin?err',
         templateUrl: 'modules/users/client/views/authentication/signin.client.view.html'
