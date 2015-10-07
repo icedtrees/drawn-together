e225187992ec30b179b810662d72228afa5cf289
cloudier <cldtu@yahoo.com.au>
2015-10-07 19:21:24 +1100

    Updated grunt to drop database when starting in development environment in case of user schema changes.

--- gruntfile.js
+++ gruntfile.js
@@ -252,17 +252,39 @@ module.exports = function (grunt) {
 
     // Connect to database
     mongoose.connect(function (db) {
-      db.mongoose.connection.on('open', function () {
-        db.mongoose.connection.db.dropDatabase(function(err) {
-          if(err) {
-            console.log(err);
-          } else {
-            console.log('Successfully dropped db');
-          }
-          db.mongoose.connection.close(done);
-        });
-      });
       done();
+    });
+  });
+
+  grunt.registerTask('mongo-drop', 'Drop mongodb database.', function() {
+    var options,
+        mongoose = require('mongoose'),
+        done = this.async(),
+        db;
+
+    options = this.options({
+      dbname: 'mean-dev',
+      host: 'localhost'
+    });
+
+    grunt.verbose.writeflags(options, 'Options');
+
+    db = mongoose.connect('mongodb://' + options.host + '/' + options.dbname, function(err) {
+      if (err) {
+        grunt.log.writeln('Could not connect to mongodb -- is mongo running?');
+        done(err);
+      } else {
+        grunt.log.writeln('Open db connection');
+        db.connection.db.dropDatabase(function(err) {
+          if (err) {
+            grunt.log.writeln('Could not drop database');
+            done(err);
+          } else {
+            grunt.log.writeln('Database dropped');
+            done();
+          }
+        });
+      }
     });
   });
 
@@ -300,7 +322,7 @@ module.exports = function (grunt) {
   grunt.registerTask('coverage', ['env:test', 'lint', 'mocha_istanbul:coverage']);
 
   // Run the project in development mode
-  grunt.registerTask('default', ['install', 'env:dev', 'lint', 'mkdir:upload', 'copy:localConfig', 'concurrent:default']);
+  grunt.registerTask('default', ['install', 'env:dev', 'mongo-drop', 'lint', 'mkdir:upload', 'copy:localConfig', 'concurrent:default']);
 
   // Run the project in debug mode
   grunt.registerTask('debug', ['install', 'env:dev', 'lint', 'mkdir:upload', 'copy:localConfig', 'concurrent:debug']);
