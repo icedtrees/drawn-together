703db0ea1f16c452ee491f6a9ca5a2247d06527d
delete12 <delete-12@hotmail.com>
2015-10-16 02:43:40 +1100

    indentation

--- modules/game/server/helpers/game.server.utils.js
+++ modules/game/server/helpers/game.server.utils.js
@@ -4,21 +4,21 @@ var blacklist = ['and', 'or', 'so', 'as', 'if', 'the', 'a', 'an', 'at', 'by', 'i
 
 // Given a string, returns a set of words that aren't in the blacklist of articles/prepositions/conjunctions
 exports.importantWords = function(str, removePunctuation) {
-var words = str.split(/[ -]/);
-var wordList = [];
+  var words = str.split(/[ -]/);
+  var wordList = [];
 
-for (var i = 0; i < words.length; i++ ) {
-  // remove punctuation to prevent double counting of words like fire's and fire,s
-  if (removePunctuation) {
-    words[i] = words[i].replace(/[\W_]/g, '');
+  for (var i = 0; i < words.length; i++ ) {
+    // remove punctuation to prevent double counting of words like fire's and fire,s
+    if (removePunctuation) {
+      words[i] = words[i].replace(/[\W_]/g, '');
+    }
+
+    if ((words[i].length <= 3 && blacklist.indexOf(words[i]) !== -1) || // ignore if blacklisted
+      wordList.indexOf(words[i]) !== -1) {                              // or already in word list
+      continue;
+    }
+    wordList.push(words[i]);
   }
 
-  if ((words[i].length <= 3 && blacklist.indexOf(words[i]) !== -1) || // ignore if blacklisted
-    wordList.indexOf(words[i]) !== -1) {                              // or already in word list
-    continue;
-  }
-   wordList.push(words[i]);
-}
-
-return wordList;
+  return wordList;
 };
