63885d345248ac506048c9a476f1e69305230b21
Claudia Tu <cldtu@yahoo.com.au>
2015-10-17 22:07:58 +1100

    Routed to lobby as homepage. All join buttons go to the same game page.

--- modules/lobby/client/config/lobby.client.config.js
+++ modules/lobby/client/config/lobby.client.config.js
@@ -2,12 +2,5 @@
 
 // Configuring the Articles module
 angular.module('lobby').run(['Menus',
-  function (Menus) {
-    // Add shortcut in menu to access lobby
-    Menus.addMenuItem('topbar', {
-      title: 'Lobby',
-      state: 'lobby',
-      position: 1
-    });
-  }
+  function (Menus) {}
 ]);
