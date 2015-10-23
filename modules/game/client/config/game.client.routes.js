6869188afa35f54e57ab396b874d9adca9e054e0
Davy Mao <e_equals_mass_speed_light_squared@hotmail.com>
2015-10-24 00:30:58 +1100

    Make / route to game view

--- modules/game/client/config/game.client.routes.js
+++ modules/game/client/config/game.client.routes.js
@@ -5,7 +5,7 @@ angular.module('game').config(['$stateProvider',
   function ($stateProvider) {
     $stateProvider
       .state('game', {
-        url: '/game',
+        url: '/',
         templateUrl: 'modules/game/client/views/game.client.view.html',
         data: {
           roles: ['user', 'admin']
