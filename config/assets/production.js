b01477731f378e4088300c4a9060e51cdac0b65d
Davy Mao <davy3m@gmail.com>
2023-11-13 15:16:54 -0500

    remove more unused bower packages

--- config/assets/production.js
+++ config/assets/production.js
@@ -10,14 +10,11 @@ module.exports = {
       js: [
         'public/lib/angular/angular.min.js',
         'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
         'public/lib/angular-messages/angular-messages.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/angular-ui-utils/ui-utils.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js',
         'public/lib/angular-bootstrap-colorpicker/js/bootstrap-colorpicker-module.min.js',
-        'public/lib/angular-scroll-glue/src/scrollglue.js'
+        'public/lib/angular-scroll-glue/src/scrollglue.js',
+        'public/lib/angular-sanitize/angular-sanitize.js'
       ]
     },
     css: [
