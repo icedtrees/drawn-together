abfe2f37d50ea1bc564a6aaf0b57c9fede449afe
Davy Mao <icedtrees@users.noreply.github.com>
2023-11-20 16:46:40 -0500

    Migrate lobby page to react (#153)
    Migrate the lobby page to react. We did some hacks and built out some ways for us to use the angular code outside of angular

--- modules/client/lobby/lobby.client.module.ts
+++ modules/client/lobby/lobby.client.module.ts
@@ -2,6 +2,5 @@
 
 // Use Application configuration module to register a new module
 import {ApplicationConfiguration} from "../core/app/config";
-import './css/lobby.css'
 
 export const lobbyModule = ApplicationConfiguration.registerModule('lobby');
