ade7d5872636fa87c6fc3ca909c1fe0e286510a0
Davy Mao <davy3m@gmail.com>
2023-11-28 22:14:00 -0500

    delete a lot of now unused files and code

--- application.ts
+++ application.ts
@@ -18,8 +18,6 @@ import './modules/client/core/services/interceptors/auth.interceptor.client.serv
 import './modules/client/core/services/socket.io.client.service.ts'
 import './modules/client/game/config/game.client.config.ts'
 import './modules/client/game/config/game.client.routes.ts'
-import './modules/client/game/controllers/game.client.controller.ts'
-import './modules/client/game/directives/drawing.client.directives.ts'
 import './modules/client/game/directives/unselectable.client.directive.ts'
 import './modules/client/game/game.client.module.ts'
 import './modules/client/lobby/config/lobby.client.config.ts'
